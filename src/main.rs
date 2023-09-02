90dd683c8a7e4f89fcb4ca66cca80d83ba45bfdb
David <david.bousi@cz.ey.com>
2023-09-02 09:51:22 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let string = String::from("127.0.0.1:8080");
+    let server = Server::new(string);
+    server.run();
+}
+
+struct Server {
+    address: String,
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+       Self {
+           address
+       }
+    }
+    fn run(self) {
+
+    }
+}
