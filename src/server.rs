1078f5507c1a164cbdd3cf716755f2fd9a892ebc
David <david.bousi@cz.ey.com>
2023-09-03 07:28:33 +0200

    tcplistener implementation

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-    pub struct Server {
+    use std::net::TcpListener;
+pub struct Server {
         address: String,
     }
 
@@ -8,5 +9,14 @@
         }
         pub fn run(self) {
             println!("Listening on {}", self.address);
+            let listener = TcpListener::bind(&self.address).unwrap();
+            loop {
+                match listener.accept() {
+                    Ok((stream, _)) => {
+                        println!("OK!")
+                    }
+                    Err(e) => println!("Failed to establish a connection: {}", e),
+                }
+            }
         }
     }
