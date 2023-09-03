b72f3773e3e0a48172ec1b0ffd84498458d2ef6e
ox1david <bousi.david@gmail.com>
2023-09-03 22:14:51 +0200

    working on server

--- src/server.rs
+++ src/server.rs
@@ -1,19 +1,29 @@
     use std::net::TcpListener;
+    use std::io::Read;
+
 pub struct Server {
         address: String,
     }
+
+fn arr(a: &[u8; 5]) {
+
+}
 
     impl Server {
         pub fn new(address: String) -> Self {
             Self { address }
         }
+
         pub fn run(self) {
             println!("Listening on {}", self.address);
+
             let listener = TcpListener::bind(&self.address).unwrap();
+
             loop {
                 match listener.accept() {
-                    Ok((stream, _)) => {
-                        println!("OK!")
+                    Ok((mut stream, _)) => {
+                       let mut buffer = [0; 1024]; 
+                           stream.read(&mut buffer); 
                     }
                     Err(e) => println!("Failed to establish a connection: {}", e),
                 }
